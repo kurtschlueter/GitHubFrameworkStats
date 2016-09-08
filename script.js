fd309a3b0be02031dcc1379c6f25a7c7a7b4bd8f
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-09-07 20:15:51 -0500

    stats table shows content

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
+var frameworks = []
+
 var Httpreq = new XMLHttpRequest();
 
 Httpreq.open("GET","https://api.github.com/repos/facebook/react",false);
 Httpreq.send(null);
 var reactJSON = JSON.parse(Httpreq.response);
+frameworks[0] = reactJSON
 
 Httpreq.open("GET","https://api.github.com/repos/angular/angular.js",false);
 Httpreq.send(null);
 var angularJSON = JSON.parse(Httpreq.response);
+frameworks[1] = angularJSON
 
 Httpreq.open("GET","https://api.github.com/repos/emberjs/ember.js",false);
 Httpreq.send(null);
 var emberJSON = JSON.parse(Httpreq.response);
+frameworks[2] = emberJSON
 
 Httpreq.open("GET","https://api.github.com/repos/vuejs/vue",false);
 Httpreq.send(null);
 var vueJSON = JSON.parse(Httpreq.response);
+frameworks[3] = vueJSON
 
 window.onload = function() {
-//        //when the document is finished loading, replace everything
-//        //between the <a ...> </a> tags with the value of splitText
-//    document.getElementById("myLink").innerHTML='cat';
-// }
 
   var table = document.getElementById("myTable");
-  for (i = 1; i < 5; i++) {
-    var row = table.insertRow(i);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      var cell5 = row.insertCell(4);
-      cell1.innerHTML = "NEW CELL1";
-      cell2.innerHTML = "NEW CELL2";
-      cell3.innerHTML = "NEW CELL3";
-      cell4.innerHTML = "NEW CELL4";
-      cell5.innerHTML = "NEW CELL5";
+
+  for (i = 0; i < 4; i++) {
+    var row = table.insertRow(i+1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = "<img src=" + frameworks[i].owner.avatar_url + "width='42' height='42'>"
+    cell2.innerHTML = frameworks[i].name;
+    cell3.innerHTML = frameworks[i].subscribers_count.toString();
+    cell4.innerHTML = frameworks[i].stargazers_count.toString();
+    cell5.innerHTML = frameworks[i].forks.toString();
   }
 }
